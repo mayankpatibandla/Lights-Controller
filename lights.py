ee59154371c2600cbab5dffa615518d0c05fc23b
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-09-03 16:46:09 -0400

    LED init
    Sets all to white

---
+++ lights.py
@@ -0,0 +1,18 @@
+from rpi_ws281x import PixelStrip
+
+# LED config
+LED_COUNT = 288
+LED_PIN = 18
+LED_FREQ_HZ = 800000
+LED_DMA = 10
+LED_BRIGHTNESS = 255
+LED_INVERT = False
+LED_CHANNEL = 0
+
+if __name__ == "__main__":
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip.begin()
+
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, 0xFFFFFF)
+    
