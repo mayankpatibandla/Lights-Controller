beceb63a1bdad03a43ec065a24c0c0fdd60251ce
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-09-05 22:29:08 -0400

    Added slice functionality and more set functions

--- lights_controller.py
+++ lights_controller.py
@@ -15,14 +15,28 @@ class Controller:
     def __iter__(self):
         return self
 
-    def __setitem__(self, index: int, color: int):
-        self.strip.setPixelColor(index, color)
+    def __setitem__(self, pos: int, color: int):
+        if isinstance(pos, slice):
+            for i in range(*pos.indices(len(self))):
+                self.set(i, color)
+        else:
+            self.set(pos, color)
 
-    def __getitem__(self, index: int):
-        return self.strip.getPixelColor(index)
+    def __getitem__(self, pos: int | slice):
+        if isinstance(pos, slice):
+            return [self.strip.getPixelColor(i) for i in range(*pos.indices(len(self)))]
+        else:
+            return self.strip.getPixelColor(pos)
 
     def __next__(self):
         self.currentIter += 1
         if self.currentIter <= self.maxIter:
             return self[self.currentIter]
         raise StopIteration
+
+    def set(self, index: int, color: int):
+        self.strip.setPixelColor(index, color)
+
+    def set_all(self, color: int):
+        for i in range(len(self)):
+            self.set(i, color)
