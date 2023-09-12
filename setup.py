07db4fdf030474d061571aec0f1e9c5572f341f0
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-09-12 16:06:26 -0400

    Formatting

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup
 
-setup(name="lights_controller", url="https://github.com/mayankpatibandla/Lights-Controller", author="Mayank Patibandla", packages=["lights_controller"])
+setup(
+    name="lights_controller",
+    url="https://github.com/mayankpatibandla/Lights-Controller",
+    author="Mayank Patibandla",
+    packages=["lights_controller"]
+)
