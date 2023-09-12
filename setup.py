7a2e874505dcfabcb51534171830f6f15ef477df
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2023-09-12 16:04:09 -0400

    Fixed issue with package not being importable

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name="lights_controller", url="https://github.com/mayankpatibandla/Lights-Controller", author="Mayank Patibandla", packages=["controller"])
+setup(name="lights_controller", url="https://github.com/mayankpatibandla/Lights-Controller", author="Mayank Patibandla", packages=["lights_controller"])
